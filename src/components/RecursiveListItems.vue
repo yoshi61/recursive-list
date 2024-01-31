a2674786b3bf296f0623fafb81f2839e66bc1bdc
Hideki Yoshinaga <yoshi@Hidekis-MacBook-Pro-2.local>
2024-01-31 22:34:41 +1030

    Add indent for child items and add bullet points

--- src/components/RecursiveListItems.vue
+++ src/components/RecursiveListItems.vue
@@ -2,6 +2,8 @@
   <div v-for="item in items" :key="item.id">
     <template v-if="hasChildren(item)">
         <div>
+            <div class="spacer" :style="`width: ${level*1}rem`" />
+            <div class="bullet">▼</div>
             {{ item.name }}
         </div>
         <div>
@@ -10,6 +12,7 @@
     </template>
     <template v-else>
       <div>
+          <div class="spacer" :style="`width: ${level*1}rem`" /><div class="bullet">•</div>
           {{ item.name }}
       </div>
     </template>
@@ -38,5 +41,13 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.bullet {
+    display: inline-block;
+    text-align: center;
+    width: 1rem;
+}
+.spacer {
+    display: inline-block;
+}
 </style>
