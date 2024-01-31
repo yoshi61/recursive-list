1d86b3a0ef751552ed953ae94a15d124e6d39e93
Hideki Yoshinaga <yoshi@Hidekis-MacBook-Pro-2.local>
2024-01-31 21:41:57 +1030

    init repository

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>
+    Please build the structure you see in <code>PageTree.png</code> in the file
+    explorer.
+  </p>
+  <p>
+    The data for this structure should be loaded from <code>fetchData.js</code>.
+  </p>
+  <p>Useful icon characters are ▼ ▶ •</p>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
